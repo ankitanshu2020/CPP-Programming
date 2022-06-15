021312100f52238005ef81d1a1f9de22b27a3f6d
Ankitanshu <ankitanshu9006@gmail.com>
2022-06-15 08:31:20 +0530

    up

---
+++ sum_of_two_numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n1,n2;
+    cout<< "Enter the two numbers"<<endl;
+    cin>>n1>>n2;
+    cout<<"Sum = "<<n1+n2;
+    return 0;
+    
+}
